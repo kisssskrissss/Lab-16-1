b37f688e52cba892223079e017ef1a5c098a8e25
krist <krist@LAPTOP-HIQLHLL6>
2022-11-27 21:08:06 +0500

    Добавьте файлы проекта.

---
+++ Lab 16 1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_16._1
+{
+    class Product
+    {
+        public int Num { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Lab 16 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_16._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Product[] products = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите код товара");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите название товара");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите цену товара");
+                int price = Convert.ToInt32(Console.ReadLine());
+                products[i] = new Product() { Num = num, Name = name, Price = price };
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(products, options);
+            using (StreamWriter sw = new StreamWriter("../../../Products.json"))
+            {
+                sw.WriteLine(jsonString);
+            }
+        }
+    }
+}
